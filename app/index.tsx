2182024df604d0f3ceed84ef17f527f04a7a5037
ItsME-TM <thushanmalaka123@gmail.com>
2025-05-28 19:03:23 +0530

    Setup and work with react router

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,16 @@
 import { StyleSheet, View } from "react-native";
 import { ShoppingListItems } from "../components/ShoppingListItems";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href={"/counter"}
+        style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}
+      >
+        Go to Counter
+      </Link>
       <ShoppingListItems name="coffee" />
       <ShoppingListItems name="tea" isCompleted />
       <ShoppingListItems name="Orange Juice" />

5023e0771cdfdd1a59fd4adcce10a1552f4242f0
ItsME-TM <thushanmalaka123@gmail.com>
2025-05-28 20:26:51 +0530

    Add icons to Bottom Tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,38 @@
 import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Shopping List" }} />
-      <Tabs.Screen name="idea" options={{ title: "Idea" }} />
-      <Tabs.Screen name="counter" options={{ title: "Counter" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="plus" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }

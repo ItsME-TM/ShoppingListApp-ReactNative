dfd945f8b2b3fafa9ab38b35f03aaee26e7d2171
ItsME-TM <thushanmalaka123@gmail.com>
2025-05-28 19:17:06 +0530

    Change stack navigation to Bottom Tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-      <Stack.Screen name="idea" options={{ title: "Idea" }} />
-      <Stack.Screen
-        name="counter"
-        options={{ title: "Counter", presentation: "modal", animation: "fade" }}
-      />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: "Shopping List" }} />
+      <Tabs.Screen name="idea" options={{ title: "Idea" }} />
+      <Tabs.Screen name="counter" options={{ title: "Counter" }} />
+    </Tabs>
   );
 }

517653fbf2d350e53f78ba246633de8c0f8edd97
ItsME-TM <thushanmalaka123@gmail.com>
2025-06-01 18:39:10 +0530

    Use text inputs and use handle Submit function

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,51 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { ShoppingListItems } from "../components/ShoppingListItems";
+import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemsType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemsType[] = [
+  { id: "1", name: "coffee" },
+  { id: "2", name: "tea" },
+  { id: "3", name: "Orange Juice" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemsType[]>(initialList);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList = [
+        {
+          id: new Date().toTimeString(),
+          name: value,
+        },
+        ...shoppingList,
+      ];
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItems name="coffee" />
-      <ShoppingListItems name="tea" isCompleted />
-      <ShoppingListItems name="Orange Juice" />
+      <TextInput
+        placeholder="Add a new item..."
+        style={styles.textInput}
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItems name={item.name} key={item.id} />
+      ))}
     </View>
   );
 }
@@ -15,6 +54,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
   },
 });
